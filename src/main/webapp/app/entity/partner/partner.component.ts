2cca783e74135b509f0dd25cd7478a0c785f53ad
arunas <arunas.stonis@gmail.com>
2016-11-30 10:55:40 +0200

    generated partner entity with angular-cli

---
+++ src/main/webapp/app/entity/partner/partner.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-partner',
+  templateUrl: './partner.component.html',
+  styleUrls: ['./partner.component.css'],
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.Default
+})
+export class PartnerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
